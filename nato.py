c1ad8dd17cc7a0e658a2e6c0bbd6883e7478bce5
jdav892 <jadavila9@gmail.com>
2024-06-30 14:00:32 -0400

    error handling

--- nato.py
+++ nato.py
@@ -6,10 +6,15 @@ alpha_data_frame = pandas.DataFrame(alpha_data)
 
 alpha_dict = {row.letter : row.code for (index, row) in alpha_data_frame.iterrows()}
 
-user_input = input("Enter a word ").upper()
 
-user_result = [alpha_dict[letter] for letter in user_input]
-print(user_result)
+def generate_phonetic():
+    user_input = input("Enter a word ").upper()
+    try:
+        user_result = [alpha_dict[letter] for letter in user_input]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please")
+        generate_phonetic()
+    else:
+        print(user_result)
 
-
-        
+generate_phonetic()
